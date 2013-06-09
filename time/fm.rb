0c6432f3542eff30aeaa03993716357ce6fc755f
ma2dev <e164num@gmail.com>
2013-06-09 14:42:51 +0900

    update Timeを使ってみた。

--- time/fm.rb
+++ time/fm.rb
@@ -1,1 +1,177 @@
-p "test"
+d1 = Time.local(2013,2,7,1,30)
+d2 = Time.local(2013,1,7,1,30)
+p d1 - d2
+
+d11 = Time.local(2013,6,7,2,0)
+d12 = Time.local(2013,6,9,0,0)
+d13 = Time.local(2013,2,2,0,0)
+d14 = Time.local(2013,5,2,0,0)
+d15 = Time.local(2016,3,2,0,0)
+d16 = Time.local(2013,4,2,0,0)
+d17 = Time.local(2014,2,1,0,0)
+d21 = Time.local(2013,6,7,0,59)
+d22 = Time.local(2013,6,7,23,59)
+d23 = Time.local(2013,1,31,23,59)
+d24 = Time.local(2013,4,30,23,59)
+d25 = Time.local(2016,2,29,23,59)
+d26 = Time.local(2013,3,31,23,59)
+d27 = Time.local(2013,12,31,23,59)
+
+p d11 - d21
+p d12 - d22
+p d13 - d23
+p d14 - d24
+p d15 - d25
+p d16 - d26
+p d17 - d27
+
+d111 = Time.local(2013,6,7,1,30)
+d112 = Time.local(2013,6,7,1,30)
+d113 = Time.local(2013,6,7,1,30)
+d114 = Time.local(2013,1,7,1,30)
+d115 = Time.local(2013,6,7,1,30)
+d211 = Time.local(2013,6,7,1,31)
+d212 = Time.local(2013,6,7,2,30)
+d213 = Time.local(2013,6,8,1,30)
+d214 = Time.local(2013,2,7,1,30)
+d215 = Time.local(2014,6,7,1,30)
+
+p d111 - d211
+p d112 - d212
+p d113 - d213
+p d114 - d214
+p d115 - d215
+
+
+x1 = Time.local(2013,1,1,1,10)
+x2 = Time.local(2013,2,1,1,10)
+x3 = Time.local(2013,3,1,1,10)
+x4 = Time.local(2016,3,1,1,10)
+x5 = Time.local(2013,4,1,1,10)
+x6 = Time.local(2013,5,1,1,10)
+x7 = Time.local(2013,6,1,1,10)
+x8 = Time.local(2013,7,1,1,10)
+x9 = Time.local(2013,8,1,1,10)
+x10 = Time.local(2013,9,1,1,10)
+x11 = Time.local(2013,10,1,1,10)
+x12 = Time.local(2013,11,1,1,10)
+x13 = Time.local(2013,12,1,1,10)
+
+y1 = Time.local(2012,12,31,12,30)
+y2 = Time.local(2013,1,31,12,30)
+y3 = Time.local(2013,2,28,12,30)
+y4 = Time.local(2016,2,29,12,30)
+y5 = Time.local(2013,3,31,12,30)
+y6 = Time.local(2013,4,30,12,30)
+y7 = Time.local(2013,5,31,12,30)
+y8 = Time.local(2013,6,30,12,30)
+y9 = Time.local(2013,7,31,12,30)
+y10 = Time.local(2013,8,31,12,30)
+y11 = Time.local(2013,9,30,12,30)
+y12 = Time.local(2013,10,31,12,30)
+y13 = Time.local(2013,11,30,12,30)
+
+p " "
+p x1 - y1
+p x2 - y2
+p x3 - y3
+p x4 - y4
+p x5 - y5
+p x6 - y6
+p x7 - y7
+p x8 - y8
+p x9 - y9
+p x10 - y10
+p x11 - y11
+p x12 - y12
+p x13 - y13
+
+
+x1 = Time.local(2013,1,1,12,30)
+x2 = Time.local(2013,2,1,12,30)
+x3 = Time.local(2013,3,1,12,30)
+x4 = Time.local(2016,3,1,12,30)
+x5 = Time.local(2013,4,1,12,30)
+x6 = Time.local(2013,5,1,12,30)
+x7 = Time.local(2013,6,1,12,30)
+x8 = Time.local(2013,7,1,12,30)
+x9 = Time.local(2013,8,1,12,30)
+x10 = Time.local(2013,9,1,12,30)
+x11 = Time.local(2013,10,1,12,30)
+x12 = Time.local(2013,11,1,12,30)
+x13 = Time.local(2013,12,1,12,30)
+
+y1 = Time.local(2012,12,31,1,10)
+y2 = Time.local(2013,1,31,1,10)
+y3 = Time.local(2013,2,28,1,10)
+y4 = Time.local(2016,2,29,1,10)
+y5 = Time.local(2013,3,31,1,10)
+y6 = Time.local(2013,4,30,1,10)
+y7 = Time.local(2013,5,31,1,10)
+y8 = Time.local(2013,6,30,1,10)
+y9 = Time.local(2013,7,31,1,10)
+y10 = Time.local(2013,8,31,1,10)
+y11 = Time.local(2013,9,30,1,10)
+y12 = Time.local(2013,10,31,1,10)
+y13 = Time.local(2013,11,30,1,10)
+
+p " "
+p x1 - y1
+p x2 - y2
+p x3 - y3
+p x4 - y4
+p x5 - y5
+p x6 - y6
+p x7 - y7
+p x8 - y8
+p x9 - y9
+p x10 - y10
+p x11 - y11
+p x12 - y12
+p x13 - y13
+
+
+x1 = Time.local(2013,1,1,1,10)
+x2 = Time.local(2013,2,1,1,10)
+x3 = Time.local(2013,3,1,1,10)
+x4 = Time.local(2016,3,1,1,10)
+x5 = Time.local(2013,4,1,1,10)
+x6 = Time.local(2013,5,1,1,10)
+x7 = Time.local(2013,6,1,1,10)
+x8 = Time.local(2013,7,1,1,10)
+x9 = Time.local(2013,8,1,1,10)
+x10 = Time.local(2013,9,1,1,10)
+x11 = Time.local(2013,10,1,1,10)
+x12 = Time.local(2013,11,1,1,10)
+x13 = Time.local(2013,12,1,1,10)
+
+y1 = Time.local(2012,12,31,1,10)
+y2 = Time.local(2013,1,31,1,10)
+y3 = Time.local(2013,2,28,1,10)
+y4 = Time.local(2016,2,29,1,10)
+y5 = Time.local(2013,3,31,1,10)
+y6 = Time.local(2013,4,30,1,10)
+y7 = Time.local(2013,5,31,1,10)
+y8 = Time.local(2013,6,30,1,10)
+y9 = Time.local(2013,7,31,1,10)
+y10 = Time.local(2013,8,31,1,10)
+y11 = Time.local(2013,9,30,1,10)
+y12 = Time.local(2013,10,31,1,10)
+y13 = Time.local(2013,11,30,1,10)
+
+
+p " "
+p x1 - y1
+p x2 - y2
+p x3 - y3
+p x4 - y4
+p x5 - y5
+p x6 - y6
+p x7 - y7
+p x8 - y8
+p x9 - y9
+p x10 - y10
+p x11 - y11
+p x12 - y12
+p x13 - y13
+
